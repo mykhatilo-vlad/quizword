3432e2d21ab51ea5b56e826f73e819e914de426d
Vlad Mykhatilo <57641289+mykhatilo-vlad@users.noreply.github.com>
2023-04-29 17:08:17 +0300

    add irregular verbs

--- src/data/words.js
+++ src/data/words.js
@@ -1,44 +1,874 @@
 export default [
     {
-        en: "pain",
-        ua: 'біль',
-        column: 'day',
-        id: 0
+        "en": "smell smelt smelt ( smelled smelled)",
+        "ua": "нюхати, мати запах",
+        "column": "day",
+        "id": 0
     },
     {
-        en: 'reduce',
-        ua: 'зменшити',
-        column: 'day',
-        id: 1
+        "en": "tell told told",
+        "ua": "розповідати",
+        "column": "day",
+        "id": 1
     },
     {
-        en: 'crib',
-        ua: 'ліжечко',
-        column: 'day',
-        id: 2
+        "en": "Bend bent bent",
+        "ua": "Зігнути",
+        "column": "day",
+        "id": 2
     },
     {
-        en: 'outlet',
-        ua: 'розетка',
-        column: 'day',
-        id: 3
+        "en": "Weep wept wept",
+        "ua": "Плакати,тужити",
+        "column": "day",
+        "id": 3
     },
     {
-        en: 'negotiate',
-        ua: 'домовлятися',
-        column: 'day',
-        id: 4
+        "en": "fly flew flown",
+        "ua": "літати",
+        "column": "day",
+        "id": 4
     },
     {
-        en: 'plastering',
-        ua: 'оштукатурювання',
-        column: 'day',
-        id: 5
+        "en": "spit spit/spat",
+        "ua": "плюнути зневажати",
+        "column": "day",
+        "id": 5
     },
     {
-        en: 'plasterer',
-        ua: 'штукатур',
-        column: 'day',
-        id: 6
+        "en": "bring brought brought",
+        "ua": "приносити",
+        "column": "day",
+        "id": 6
     },
-]
+    {
+        "en": "build built built",
+        "ua": "будувати",
+        "column": "day",
+        "id": 7
+    },
+    {
+        "en": "burn burnt burnt ( burned burned)burst",
+        "ua": "горіти",
+        "column": "day",
+        "id": 8
+    },
+    {
+        "en": "buy bought bought",
+        "ua": "купувати",
+        "column": "day",
+        "id": 9
+    },
+    {
+        "en": "catch caught caught",
+        "ua": "ловити",
+        "column": "day",
+        "id": 10
+    },
+    {
+        "en": "dig dug dug",
+        "ua": "копати",
+        "column": "day",
+        "id": 11
+    },
+    {
+        "en": "dream dreamt dreamt ( dreamed dreamed)",
+        "ua": "мріяти/бачити уві сні",
+        "column": "day",
+        "id": 12
+    },
+    {
+        "en": "feed fed fed",
+        "ua": "годувати",
+        "column": "day",
+        "id": 13
+    },
+    {
+        "en": "feel felt felt",
+        "ua": "відчувати",
+        "column": "day",
+        "id": 14
+    },
+    {
+        "en": "find found found",
+        "ua": "знаходити",
+        "column": "day",
+        "id": 15
+    },
+    {
+        "en": "hear heard heard",
+        "ua": "чути",
+        "column": "day",
+        "id": 16
+    },
+    {
+        "en": "lose lost lost",
+        "ua": "втрачати, губити, програвати",
+        "column": "day",
+        "id": 17
+    },
+    {
+        "en": "make made made",
+        "ua": "виготовляти, створювати",
+        "column": "day",
+        "id": 18
+    },
+    {
+        "en": "meet met met",
+        "ua": "зустріти, познайомитися",
+        "column": "day",
+        "id": 19
+    },
+    {
+        "en": "say said said",
+        "ua": "сказати",
+        "column": "day",
+        "id": 20
+    },
+    {
+        "en": "sell sold sold",
+        "ua": "продавати",
+        "column": "day",
+        "id": 21
+    },
+    {
+        "en": "stand stood stood",
+        "ua": "стояти",
+        "column": "day",
+        "id": 22
+    },
+    {
+        "en": "understand understood understood",
+        "ua": "розуміти",
+        "column": "day",
+        "id": 23
+    },
+    {
+        "en": "Lead led led",
+        "ua": "Очолювати",
+        "column": "day",
+        "id": 24
+    },
+    {
+        "en": "Spin spun spun",
+        "ua": "Прясти",
+        "column": "day",
+        "id": 25
+    },
+    {
+        "en": "become became become",
+        "ua": "ставати",
+        "column": "day",
+        "id": 26
+    },
+    {
+        "en": "run ran run",
+        "ua": "бігти",
+        "column": "day",
+        "id": 27
+    },
+    {
+        "en": "shine shone/shined",
+        "ua": "блистіти",
+        "column": "day",
+        "id": 28
+    },
+    {
+        "en": "have had had",
+        "ua": "мати",
+        "column": "day",
+        "id": 29
+    },
+    {
+        "en": "keep kept kept",
+        "ua": "зберігати",
+        "column": "day",
+        "id": 30
+    },
+    {
+        "en": "lay laid laid",
+        "ua": "покласти",
+        "column": "day",
+        "id": 31
+    },
+    {
+        "en": "pay paid paid",
+        "ua": "платити",
+        "column": "day",
+        "id": 32
+    },
+    {
+        "en": "send sent sent",
+        "ua": "надсилати",
+        "column": "day",
+        "id": 33
+    },
+    {
+        "en": "spend spent spent",
+        "ua": "витрачати (гроші), проводити (час)",
+        "column": "day",
+        "id": 34
+    },
+    {
+        "en": "teach taught taught",
+        "ua": "навчати",
+        "column": "day",
+        "id": 35
+    },
+    {
+        "en": "think thought thought",
+        "ua": "думати",
+        "column": "day",
+        "id": 36
+    },
+    {
+        "en": "win won won",
+        "ua": "вигравати",
+        "column": "day",
+        "id": 37
+    },
+    {
+        "en": "Bind bound bound",
+        "ua": "Зв'язувати",
+        "column": "day",
+        "id": 38
+    },
+    {
+        "en": "Bleed bled bled",
+        "ua": "Кровоточити",
+        "column": "day",
+        "id": 39
+    },
+    {
+        "en": "Breed bred bred",
+        "ua": "Розводити",
+        "column": "day",
+        "id": 40
+    },
+    {
+        "en": "Cling clung clung",
+        "ua": "Чіплятися, чіпляти",
+        "column": "day",
+        "id": 41
+    },
+    {
+        "en": "Creep crept crept",
+        "ua": "Повзти",
+        "column": "day",
+        "id": 42
+    },
+    {
+        "en": "Deal dealt dealt",
+        "ua": "Мати справу",
+        "column": "day",
+        "id": 43
+    },
+    {
+        "en": "Fight fought fought",
+        "ua": "Битися",
+        "column": "day",
+        "id": 44
+    },
+    {
+        "en": "flee fled fled",
+        "ua": "Тікати",
+        "column": "day",
+        "id": 45
+    },
+    {
+        "en": "grind ground ground",
+        "ua": "Молоти, дрібнити, перемолоти",
+        "column": "day",
+        "id": 46
+    },
+    {
+        "en": "Hang hung hung",
+        "ua": "Повісити, висіти",
+        "column": "day",
+        "id": 47
+    },
+    {
+        "en": "Hold held held",
+        "ua": "Тримати",
+        "column": "day",
+        "id": 48
+    },
+    {
+        "en": "Leave left left",
+        "ua": "Залишати",
+        "column": "day",
+        "id": 49
+    },
+    {
+        "en": "Lend lent lent",
+        "ua": "Позичати",
+        "column": "day",
+        "id": 50
+    },
+    {
+        "en": "Mislay mislaid mislaid",
+        "ua": "Загубити,покласти не на місце",
+        "column": "day",
+        "id": 51
+    },
+    {
+        "en": "Seek sought sought",
+        "ua": "Шукати, просити поради",
+        "column": "day",
+        "id": 52
+    },
+    {
+        "en": "Shoot shot shot",
+        "ua": "Стріляти",
+        "column": "day",
+        "id": 53
+    },
+    {
+        "en": "Sit sat sat",
+        "ua": "Сидіти",
+        "column": "day",
+        "id": 54
+    },
+    {
+        "en": "Sleep slept slept",
+        "ua": "Спати",
+        "column": "day",
+        "id": 55
+    },
+    {
+        "en": "Slide slid slid",
+        "ua": "Ковзати",
+        "column": "day",
+        "id": 56
+    },
+    {
+        "en": "Stick stuck stuck",
+        "ua": "Липнути, зклеювати, приклеювати",
+        "column": "day",
+        "id": 57
+    },
+    {
+        "en": "Sting stung stung",
+        "ua": "Жалити",
+        "column": "day",
+        "id": 58
+    },
+    {
+        "en": "Sweep swept swept",
+        "ua": "Замітати",
+        "column": "day",
+        "id": 59
+    },
+    {
+        "en": "Swing swung swung",
+        "ua": "Гойдатись",
+        "column": "day",
+        "id": 60
+    },
+    {
+        "en": "Wind wound wound",
+        "ua": "Намотувати",
+        "column": "day",
+        "id": 61
+    },
+    {
+        "en": "cost cost cost",
+        "ua": "коштувати",
+        "column": "day",
+        "id": 62
+    },
+    {
+        "en": "cut cut cut",
+        "ua": "різати",
+        "column": "day",
+        "id": 63
+    },
+    {
+        "en": "hurt hurt hurt",
+        "ua": "поранитися",
+        "column": "day",
+        "id": 64
+    },
+    {
+        "en": "put put put",
+        "ua": "класти",
+        "column": "day",
+        "id": 65
+    },
+    {
+        "en": "read read read",
+        "ua": "читати",
+        "column": "day",
+        "id": 66
+    },
+    {
+        "en": "shut",
+        "ua": "закривати",
+        "column": "day",
+        "id": 67
+    },
+    {
+        "en": "bet bet bet",
+        "ua": "битися об заклад",
+        "column": "day",
+        "id": 68
+    },
+    {
+        "en": "bid",
+        "ua": "просити",
+        "column": "day",
+        "id": 69
+    },
+    {
+        "en": "broadcast,spread",
+        "ua": "поширювати",
+        "column": "day",
+        "id": 70
+    },
+    {
+        "en": "burst",
+        "ua": "горіти",
+        "column": "day",
+        "id": 71
+    },
+    {
+        "en": "forecast",
+        "ua": "прогнозувати",
+        "column": "day",
+        "id": 72
+    },
+    {
+        "en": "hit",
+        "ua": "ударити",
+        "column": "day",
+        "id": 73
+    },
+    {
+        "en": "rid",
+        "ua": "звільняти радувати",
+        "column": "day",
+        "id": 74
+    },
+    {
+        "en": "set",
+        "ua": "поміщати",
+        "column": "day",
+        "id": 75
+    },
+    {
+        "en": "slit",
+        "ua": "розрізати",
+        "column": "day",
+        "id": 76
+    },
+    {
+        "en": "split",
+        "ua": "розколоти",
+        "column": "day",
+        "id": 77
+    },
+    {
+        "en": "thrust",
+        "ua": "штовхати​",
+        "column": "day",
+        "id": 78
+    },
+    {
+        "en": "upset",
+        "ua": "засмучувати",
+        "column": "day",
+        "id": 79
+    },
+    {
+        "en": "ride rode ridden",
+        "ua": "їхати верхи або на велосипеді",
+        "column": "day",
+        "id": 80
+    },
+    {
+        "en": "speak spoke spoken",
+        "ua": "розмовляти",
+        "column": "day",
+        "id": 81
+    },
+    {
+        "en": "take took taken",
+        "ua": "брати​",
+        "column": "day",
+        "id": 82
+    },
+    {
+        "en": "wake woke woken (waked, waked)",
+        "ua": "будити , прокидатися",
+        "column": "day",
+        "id": 83
+    },
+    {
+        "en": "write wrote written",
+        "ua": "писати",
+        "column": "day",
+        "id": 84
+    },
+    {
+        "en": "arise arose arisen",
+        "ua": "виникати",
+        "column": "day",
+        "id": 85
+    },
+    {
+        "en": "forbid forbade forbidden",
+        "ua": "забороняти",
+        "column": "day",
+        "id": 86
+    },
+    {
+        "en": "freeze froze frozen",
+        "ua": "заморожувати",
+        "column": "day",
+        "id": 87
+    },
+    {
+        "en": "hide hid hidden",
+        "ua": "ховати",
+        "column": "day",
+        "id": 88
+    },
+    {
+        "en": "mistake mistook mistaken",
+        "ua": "помилятися",
+        "column": "day",
+        "id": 89
+    },
+    {
+        "en": "rise rose rissen",
+        "ua": "підніматися",
+        "column": "day",
+        "id": 90
+    },
+    {
+        "en": "shake shook shaken",
+        "ua": "трясти",
+        "column": "day",
+        "id": 91
+    },
+    {
+        "en": "steal stole stolen",
+        "ua": "красти",
+        "column": "day",
+        "id": 92
+    },
+    {
+        "en": "bite bit bitten",
+        "ua": "кусати",
+        "column": "day",
+        "id": 93
+    },
+    {
+        "en": "break broke broken",
+        "ua": "ламати",
+        "column": "day",
+        "id": 94
+    },
+    {
+        "en": "choose chose chosen",
+        "ua": "вибирати",
+        "column": "day",
+        "id": 95
+    },
+    {
+        "en": "eat ate eaten",
+        "ua": "їсти",
+        "column": "day",
+        "id": 96
+    },
+    {
+        "en": "fall fell fallen",
+        "ua": "падати",
+        "column": "day",
+        "id": 97
+    },
+    {
+        "en": "forget forgot forgotten",
+        "ua": "забувати",
+        "column": "day",
+        "id": 98
+    },
+    {
+        "en": "forgive forgave forgiven",
+        "ua": "пробачати",
+        "column": "day",
+        "id": 99
+    },
+    {
+        "en": "give gave given",
+        "ua": "давати",
+        "column": "day",
+        "id": 100
+    },
+    {
+        "en": "blow blew blown",
+        "ua": "дути",
+        "column": "day",
+        "id": 101
+    },
+    {
+        "en": "know knew known",
+        "ua": "знати",
+        "column": "day",
+        "id": 102
+    },
+    {
+        "en": "see saw seen",
+        "ua": "бачити",
+        "column": "day",
+        "id": 103
+    },
+    {
+        "en": "show showed shown(showed)",
+        "ua": "показувати",
+        "column": "day",
+        "id": 104
+    },
+    {
+        "en": "throw threw thrownforsake forsook forsakenfling flung flungcast",
+        "ua": "кидати",
+        "column": "day",
+        "id": 105
+    },
+    {
+        "en": "wear wore worn",
+        "ua": "носити одяг",
+        "column": "day",
+        "id": 106
+    },
+    {
+        "en": "begin began begun",
+        "ua": "починати",
+        "column": "day",
+        "id": 107
+    },
+    {
+        "en": "draw drew drawn",
+        "ua": "малювати",
+        "column": "day",
+        "id": 108
+    },
+    {
+        "en": "grow grew grown",
+        "ua": "рости",
+        "column": "day",
+        "id": 109
+    },
+    {
+        "en": "come came come",
+        "ua": "прийти",
+        "column": "day",
+        "id": 110
+    },
+    {
+        "en": "beat beat beaten",
+        "ua": "бити",
+        "column": "day",
+        "id": 111
+    },
+    {
+        "en": "be was/were been",
+        "ua": "бути",
+        "column": "day",
+        "id": 112
+    },
+    {
+        "en": "bear, bore, borne/born",
+        "ua": "видержувати ,наплоджувати",
+        "column": "day",
+        "id": 113
+    },
+    {
+        "en": "do did done",
+        "ua": "робити",
+        "column": "day",
+        "id": 114
+    },
+    {
+        "en": "fit fit/fitted fit/fitted",
+        "ua": "пристосовуватися",
+        "column": "day",
+        "id": 115
+    },
+    {
+        "en": "get got gotten/got",
+        "ua": "отримувати",
+        "column": "day",
+        "id": 116
+    },
+    {
+        "en": "go went gone",
+        "ua": "йти",
+        "column": "day",
+        "id": 117
+    },
+    {
+        "en": "kneel kneeled/knelt kneeled/knelt",
+        "ua": "ставати на коліна",
+        "column": "day",
+        "id": 118
+    },
+    {
+        "en": "lean leaned/leant",
+        "ua": "спиратися",
+        "column": "day",
+        "id": 119
+    },
+    {
+        "en": "leap leaped/leapt",
+        "ua": "стрибати",
+        "column": "day",
+        "id": 120
+    },
+    {
+        "en": "learn learned/learnt",
+        "ua": "вчити",
+        "column": "day",
+        "id": 121
+    },
+    {
+        "en": "let",
+        "ua": "дозволятии",
+        "column": "day",
+        "id": 122
+    },
+    {
+        "en": "lie lay lain",
+        "ua": "брехати",
+        "column": "day",
+        "id": 123
+    },
+    {
+        "en": "light lighted/lit",
+        "ua": "освітлювати",
+        "column": "day",
+        "id": 124
+    },
+    {
+        "en": "mean meant meant",
+        "ua": "мати на увазі",
+        "column": "day",
+        "id": 125
+    },
+    {
+        "en": "ring rang rung",
+        "ua": "дзвеніти дзвонити",
+        "column": "day",
+        "id": 126
+    },
+    {
+        "en": "shrink shrank/shrunk shrunk",
+        "ua": "збігатися",
+        "column": "day",
+        "id": 127
+    },
+    {
+        "en": "sing sang sung",
+        "ua": "співати",
+        "column": "day",
+        "id": 128
+    },
+    {
+        "en": "sink sank sunk",
+        "ua": "тонути",
+        "column": "day",
+        "id": 129
+    },
+    {
+        "en": "speed sped/speeded",
+        "ua": "поспішати",
+        "column": "day",
+        "id": 130
+    },
+    {
+        "en": "spell spelled/spelt",
+        "ua": "зачаровувати",
+        "column": "day",
+        "id": 131
+    },
+    {
+        "en": "spill spilled/spilt",
+        "ua": "заливати",
+        "column": "day",
+        "id": 132
+    },
+    {
+        "en": "spoil spoiled/spoilt",
+        "ua": "псувати",
+        "column": "day",
+        "id": 133
+    },
+    {
+        "en": "spring sprang/sprung sprung",
+        "ua": "брати початок , вистрибувати",
+        "column": "day",
+        "id": 134
+    },
+    {
+        "en": "stink stank/stunk stunk",
+        "ua": "смердіти",
+        "column": "day",
+        "id": 135
+    },
+    {
+        "en": "strike struck struck/striken",
+        "ua": "вдарити",
+        "column": "day",
+        "id": 136
+    },
+    {
+        "en": "strive strove/strived striven/strived",
+        "ua": "прагнути",
+        "column": "day",
+        "id": 137
+    },
+    {
+        "en": "string strung",
+        "ua": "зав'язувати",
+        "column": "day",
+        "id": 138
+    },
+    {
+        "en": "swear swore sworn",
+        "ua": "лаятись",
+        "column": "day",
+        "id": 139
+    },
+    {
+        "en": "swim swam swum",
+        "ua": "плисти",
+        "column": "day",
+        "id": 140
+    },
+    {
+        "en": "tear tore torn",
+        "ua": "вдирати",
+        "column": "day",
+        "id": 141
+    },
+    {
+        "en": "undertake undertook undertaken",
+        "ua": "взятися",
+        "column": "day",
+        "id": 142
+    },
+    {
+        "en": "weave wove woven",
+        "ua": "плести ,вити",
+        "column": "day",
+        "id": 143
+    },
+    {
+        "en": "withdraw withdrew withdrawn",
+        "ua": "вилучити",
+        "column": "day",
+        "id": 144
+    }
+];
+
+
